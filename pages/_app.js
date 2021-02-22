7fb3a44d0437cfe16983ad359ab71afe207145cd
Sherdil Khan <khansherdilprivate@gmail.com>
2021-02-22 15:54:55 +0100

    added some otehr pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import Link from "next/Link";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/">
+              <a>Home</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/about">
+              <a>About</a>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/about.js
+++ pages/about.js
@@ -1,14 +1,14 @@
 import Head from "next/head";
 
-export default function Home() {
+export default function About() {
   return (
     <div>
       <Head>
-        <title>Blog</title>
+        <title>About</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <h1>blog</h1>
+        <h1>About</h1>
       </main>
     </div>
   );
