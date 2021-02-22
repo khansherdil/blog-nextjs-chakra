3aae9060dbbddd916e83c9e91d4d51a231286066
Sherdil Khan <khansherdilprivate@gmail.com>
2021-02-22 17:32:09 +0100

    added full functionality with slugs

---
+++ pages/blog/[slug].js
@@ -0,0 +1,41 @@
+import { formatWithValidation } from "next/dist/next-server/lib/utils";
+import Head from "next/head";
+import blogPosts from "../../lib/data";
+
+export default function BlogPage({ title, date, content }) {
+  return (
+    <div>
+      <Head>
+        <title>{title}</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <h1>1st {title}</h1>
+        <p>{date}</p>
+        <div>{content}</div>
+      </main>
+    </div>
+  );
+}
+
+export async function getStaticProps(context) {
+  console.log(context);
+  const { params } = context;
+  return {
+    props: blogPosts.find((item) => item.slug === params.slug),
+  };
+}
+
+export async function getStaticPaths() {
+  const foo = {
+    paths: blogPosts.map((item) => ({
+      params: {
+        slug: item.slug,
+      },
+    })),
+    fallback: false,
+  };
+
+  console.log(JSON.stringify(foo, null, ""));
+  return foo;
+}
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import Head from "next/head";
+import blogPosts from "../lib/data";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -10,6 +12,20 @@ export default function Home() {
       <main>
         <h1>blog</h1>
       </main>
+
+      <div>
+        {blogPosts.map((item) => (
+          <div key={item.slug}>
+            <div>
+              <Link href={`/blog/${item.slug}`}>
+                <a>{item.title}</a>
+              </Link>
+            </div>
+            <div>{item.date.toString()}</div>
+            <div>{item.content}</div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
